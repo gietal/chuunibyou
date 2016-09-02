4130cb150e0c305368f6562de4e2d95bc95c1f85
gietal <gieta.laksmana@hotmail.com>
2016-09-01 22:38:14 -0700

    movement relative to camera

---
+++ chuunibyou/Assets/Scripts/Rikka/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour 
+{
+    GameObject mainCamera;
+    Rigidbody rigidBody;
+
+    public float moveSpeed = 5;
+
+    void Awake()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        Debug.AssertFormat(mainCamera != null, "no main camera");
+
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        var hMovement = CrossPlatformInputManager.GetAxis("Horizontal");
+        var vMovement = CrossPlatformInputManager.GetAxis("Vertical");
+
+        // find movement direction relative to the camera
+        // flatten the cam's forward and right vector to the xz axis
+        var camForward = Vector3.Scale(mainCamera.transform.forward, new Vector3(1, 0, 1)).normalized;
+        var camRight = Vector3.Scale(mainCamera.transform.right, new Vector3(1, 0, 1)).normalized;
+        var moveDirection = vMovement*camForward + hMovement*camRight;
+        rigidBody.velocity = moveDirection * moveSpeed;
+
+        // update rotation to look at the move direction
+        transform.LookAt(transform.position + moveDirection);
+    }
+}
